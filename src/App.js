0feb045b0b59c5d810c548493ab96eea3dcdbb47
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-06-15 21:16:15 +0100

    eslint prettier and basic navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello from React!</h1>
+        <Navbar title="Grauniad Reader" icon="far fa-newspaper" />
       </div>
     );
+
+    // USING CREATE ELEMENT
+    // return React.createElement(
+    //   "div",
+    //   { className: "App" },
+    //   React.createElement("h1", null, "Hello from React!")
+    // );
   }
 }
 
